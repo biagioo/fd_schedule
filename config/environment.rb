51d093f030121129b34e4f136288c43d68ebede6
Biagio   Mendolia <mendolia2328@gmail.com>
2020-03-03 01:19:30 +0000

    organized files/folders

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+requrie 'open-uri'
+
+require_relative '../lib/fd_schedule/scraper'
+require_relative '../lib/fd_schedule/events'
+require_relative '../lib/fd_schedule/cli'
+require_relative '../lib/fd_schedule/version'
