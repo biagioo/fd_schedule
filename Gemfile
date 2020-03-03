57891f852179c24829ec6ddca67c1523f6d6c9aa
Biagio   Mendolia <mendolia2328@gmail.com>
2020-03-03 01:07:48 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in fd_schedule.gemspec
+gemspec
---
+++ lib/fd_schedule.rb
@@ -0,0 +1,6 @@
+require "fd_schedule/version"
+
+module FdSchedule
+  class Error < StandardError; end
+  # Your code goes here...
+end
