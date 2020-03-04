fce9120ed24249ff5356826336606b5784215810
Biagio   Mendolia <mendolia2328@gmail.com>
2020-03-04 21:08:34 +0000

    minor changes/ restructuring done to the Scraper class

--- lib/fd_schedule/scraper.rb
+++ lib/fd_schedule/scraper.rb
@@ -1,20 +1,12 @@
 class Scraper
-  #attr_accessor :name, :date, :league, :location, :details
-  @@all = []
-  def self.names 
-    doc = Nokogiri::HTML(open("https://www.formulad.com/schedule"))
-    event_names = doc.css(".event-summary .name") # 9 events
-    event_names.each.with_index{ |name, i| puts "#{i+1} -- #{name.text}"}
-    end
   
-  def self.more_info
+  def self.scrape_info
     events = []
     html = "https://www.formulad.com/schedule"
     doc = Nokogiri::HTML(open(html))
     events_sum = doc.css("div.card.card-event .event-summary")
     binding.pry
     events_sum.each do |event_sum|
-      
       event = {
         :name => event_sum.css(".name").text,
         :date => event_sum.css(".dates").text,
@@ -22,10 +14,19 @@ class Scraper
         :location => event_sum.css(".location").text,
         :details => html.gsub("/schedule","") + event_sum.css("a").attribute("href").value
       }
-      events << event
+      #Events.new(name, date, league, location, details)
+      #each event needs to be an instance of the Events class. When this becomes true, Events.all will hold 9 instances(theyre are 9 events)
+      # and will be assigned the approiate attr_accessor values
     end
-    
   end 
-  
-  
+
 end 
+
+
+
+
+  # def self.scrape_events 
+  #   doc = Nokogiri::HTML(open("https://www.formulad.com/schedule"))
+  #   event_names = doc.css(".event-summary .name") # 9 events
+  #   event_names.each.with_index{ |name, i| puts "#{i+1} -- #{name.text}"}
+  # end
