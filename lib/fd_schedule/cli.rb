061ed9794b65cc75b81725480ecb64207cde0032
Biagio   Mendolia <mendolia2328@gmail.com>
2020-03-05 19:32:58 +0000

    second level of CLI is working now

--- lib/fd_schedule/cli.rb
+++ lib/fd_schedule/cli.rb
@@ -20,25 +20,62 @@ class CLI
   
   def schedule 
     puts "Here is the 2020 Schedule"
+    
     get_events.each.with_index do |event, i| 
-      puts "#{i+1} --  #{event.name}"
-    end 
+      puts "#{i+1} -- #{event.name}"
+    end
   end
   
   def menu
     input = nil 
     while input != "exit"
       puts "Enter the number of the event you'd like to learn more about, enter schedule to view the schedule again or type exit"
-      input = gets.strip.downcase 
+      input = gets.strip 
       case input 
-      when ""
-        puts "RD:1 info"
-      when "round 2"
-        puts "RD:2 info"
+      when "1"
+        get_events[0].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "2"
+        get_events[1].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "3"
+        get_events[2].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "4"
+        get_events[3].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "5"
+        get_events[4].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "6"
+        get_events[5].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "7"
+        get_events[6].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "8"
+        get_events[7].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "8"
+        get_events[8].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
+      when "9"
+        get_events[9].tap do |event| 
+          puts "Name: #{event.name} League: #{event.league} Date: #{event.date} Location: #{event.location} For more details: #{event.details}"
+        end
       when "schedule"
         schedule
       else 
-        puts "hmm, not sure what you mean. Try entering schedule, the round you want more information on or exit."
+        puts "hmm, not sure what you mean."
       end 
     end 
   end
